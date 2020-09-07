0972227742d9a720897ebe8cbfd2ebb9e1824680
kkim <dev.kkim@gmail.com>
2020-09-07 21:56:05 +0900

    비밀 정보 보호.

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require("./config/key");
+
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kkim:82495747@boilerplate.dbvuq.mongodb.net/boilerplate?retryWrites=true&w=majority', 
+mongoose.connect(config.mongoURI, 
 {
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useCreateIndex: true, 
-    useFindAndModify: false
+    useFindAndModify: false  
 }).then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err))
 
 
 
-app.get('/', (req,res) => res.send("Hello World!"))
+app.get('/', (req,res) => res.send("Hello World! 안녕하세요")) 
+
+
+//사용자의 정보를 서버로 전달.
+app.post("/register", (req,res) => { 
+   
+  //회원가입 할때 필요한  정보를 client에서 가져오면 
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  //mongoDB에서 오는 method
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+ 
